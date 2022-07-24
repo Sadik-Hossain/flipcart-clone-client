1bfb969ab18898b940bb9ba6eb542e6dd36e7088
sadik <po480373@gmai.com>
2022-07-24 22:54:11 +0600

    default username state cleared

--- src/context/DataProvider.jsx
+++ src/context/DataProvider.jsx
@@ -2,7 +2,7 @@ import { createContext, useState } from "react";
 export const DataContext = createContext(null);
 
 const DataProvider = ({ children }) => {
-  const [account, setAccount] = useState("james");
+  const [account, setAccount] = useState("");
   return (
     <DataContext.Provider
       // states
